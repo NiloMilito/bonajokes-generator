7017df4cbf5d67f18918ce19156d9f63c98ec5f0
root <root@lab10maq36.academicos.com>
2019-07-25 15:47:43 -0300

    projeto ionic

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { JokesService } from '../service/jokes.service';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private jokesService: JokesService, private alertController: AlertController) {}
+
+  piada : any;
+  piadas : any[];
+  url = '/assets/indice.jpeg';
+  valorInput: String;
+
+  
+
+  showjoke() {
+    this.jokesService.getAllJokes().subscribe(piadas => {
+      //this.piada = piadas[this.aleatorio(piadas)];
+      this.piadas = piadas;
+     // this.presentAlert();
+    }, erro => {
+      console.log(erro);
+    }, () => {
+      console.log('sucesso');
+    }
+    );
+  }
+
+  ngOnInit(): void{
+    this.showjoke();
+  }
+    
+  informaNome(){
+    var nome = this.valorInput.toLowerCase()
+    this.piadas = this.piadas.filter((item) => {
+      if(item.pergunta.toLowerCase().math(nome))
+      return item;   
+    });      
+  }
+
+  // filterLocations(searchTerm){
+  //   return this.data.filter((location) => {
+  //     return location.title.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+  //   });
+  // }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: this.piada.pergunta,
+      message: this.piada.resposta,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  aleatorio(piadas: any[]) {
+    return Math.floor(Math.random() * piadas.length + 1);
+  }
+
+
+}
